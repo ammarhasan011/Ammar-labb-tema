22d027d2f815f03d96a9ba362f835faa2b9f1542
Ammar Hasan <ammarhasan011@gmail.com>
2023-04-15 01:11:51 +0200

    får in sök funktionen i header

--- header.php
+++ header.php
@@ -20,18 +20,19 @@
                         <a class="logo" href="<?php echo home_url(); ?>">Labb 1</a>
                     </div>
                     <div class="col-sm-6 hidden-xs">
-                        <form id="searchform" class="searchform">
+                         <form method="get" id="searchform" class="searchform" action="<?php echo esc_url(home_url('/')); ?>">
                             <div>
-                                <label class="screen-reader-text">Sök efter:</label>
+                                <!-- <label class="screen-reader-text">Sök efter:</label>
                                 <input type="text" />
-                                <input type="submit" value="Sök" />
+                                <input type="submit" value="Sök" /> -->
                             </div>
                         </form>
                     </div>
                     <div class="col-xs-4 text-right visible-xs">
                         <div class="mobile-menu-wrap">
-                            <i class="fa fa-search"></i>
-                            <i class="fa fa-bars menu-icon"></i>
+                            <!-- <i class="fa fa-search"></i>
+                            <i class="fa fa-bars menu-icon"></i> -->
+
                         </div>
                     </div>
                 </div>
@@ -41,9 +42,10 @@
         <div class="mobile-search">
             <form id="searchform" class="searchform">
                 <div>
-                    <label class="screen-reader-text">Sök efter:</label>
+                    <?php get_search_form(); ?>
+                    <!-- <label class="screen-reader-text">Sök efter:</label>
                     <input type="text" />
-                    <input type="submit" value="Sök" />
+                    <input type="submit" value="Sök" /> -->
                 </div>
             </form>
         </div>
@@ -55,7 +57,8 @@
                         <?php wp_nav_menu(
                             array(
                                 'menu' => 'headermenu',
-                                'theme_location' => 'headermenu'
+                                'theme_location' => 'headermenu',
+                                // 'items_wrap' => '<ul class="menu"%3$s</ul>'
                             )
                         ); ?>
                     </div>
