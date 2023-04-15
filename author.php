82752106bb3e8b43cb9b8f46948acf08586215a3
Ammar Hasan <ammarhasan011@gmail.com>
2023-04-16 00:16:45 +0200

    sidebar

--- author.php
+++ author.php
@@ -47,56 +47,13 @@
                         </ul>
                         <ul role="navigation">
                             <li class="pagenav">
-                                <h2>Sidor</h2>
-                                <ul>
-                                    <li class="page_item current_page_item">
-                                        <a href="">Blogg</a>
-                                    </li>
-                                    <li class="page_item">
-                                        <a href="">Exempelsida</a>
-                                    </li>
-                                    <li class="page_item">
-                                        <a href="">Kontakt</a>
-                                    </li>
-                                    <li class="page_item page_item_has_children">
-                                        <a href="">Om mig</a>
-                                        <ul class="children">
-                                            <li class="page_item">
-                                                <a href="">Intressen</a>
-                                            </li>
-                                            <li class="page_item page_item_has_children">
-                                                <a href="">Portfolio</a>
-                                                <ul class="children">
-                                                    <li class="page_item">
-                                                        <a href="">Projekt 1</a>
-                                                    </li>
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </li>
-                                    <li class="page_item">
-                                        <a href="">Startsida</a>
-                                    </li>
-                                </ul>
+                                <?php dynamic_sidebar('sidor'); ?>
                             </li>
                             <li>
-                                <h2>Arkiv</h2>
-                                <ul>
-                                    <li>
-                                        <a href="arkiv.html">oktober 2016</a>
-                                    </li>
-                                </ul>
+                                <?php dynamic_sidebar('arkiv'); ?>
                             </li>
                             <li class="categories">
-                                <h2>Kategorier</h2>
-                                <ul>
-                                    <li class="cat-item">
-                                        <a href="">Natur</a> (1)
-                                    </li>
-                                    <li class="cat-item">
-                                        <a href="">Okategoriserade</a> (3)
-                                    </li>
-                                </ul>
+                                <?php dynamic_sidebar('kategorier'); ?>
                             </li>
                         </ul>
                     </div>
