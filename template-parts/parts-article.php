0978b4717e32186a0c5b79a84705be687d6b6d0e
Ammar Hasan <ammarhasan011@gmail.com>
2023-04-17 01:09:28 +0200

    arkiv

---
+++ template-parts/parts-article.php
@@ -0,0 +1,16 @@
+<h1><?php wp_title(''); ?></h1>
+<?php while (have_posts()) { ?>
+    <article>
+
+        <?php the_post();
+        the_post_thumbnail('large');
+        ?>
+
+        <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+        <?php get_template_part('/template-parts/parts', 'date'); ?>
+        <?php the_excerpt(); ?>
+    </article>
+<?php
+}
+?>
