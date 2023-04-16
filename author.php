f525b7dfafe508564235402f8a014d2026f78fba
Ammar Hasan <ammarhasan011@gmail.com>
2023-04-17 01:15:42 +0200

    arkiv part

--- author.php
+++ author.php
@@ -6,23 +6,7 @@
             <div class="row">
                 <div id="primary" class="col-xs-12 col-md-9">
 
-                    <h1><?php wp_title(''); ?></h1>
-                    <?php while (have_posts()) { ?>
-                        <article>
-
-                            <?php the_post();
-                            the_post_thumbnail('large');
-                            ?>
-
-                            <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-                            <?php get_template_part('/template-parts/parts', 'date'); ?>
-                            <?php the_excerpt(); ?>
-                        </article>
-                    <?php
-                    }
-                    ?>
-
+                    <?php get_template_part('/template-parts/parts', 'article'); ?>
 
                     <nav class="navigation pagination">
                         <?php echo paginate_links(); ?>
--- category.php
+++ category.php
@@ -6,22 +6,8 @@
             <div class="row">
                 <div id="primary" class="col-xs-12 col-md-9">
 
-                    <h1><?php wp_title(''); ?></h1>
-                    <?php while (have_posts()) { ?>
-                        <article>
+                    <?php get_template_part('/template-parts/parts', 'article'); ?>
 
-                            <?php the_post();
-                            the_post_thumbnail('large');
-                            ?>
-
-                            <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-
-                            <?php get_template_part('/template-parts/parts', 'date'); ?>
-                            <?php the_excerpt(); ?>
-                        </article>
-                    <?php
-                    }
-                    ?>
 
 
                     <nav class="navigation pagination">
